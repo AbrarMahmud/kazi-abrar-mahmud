09732e7818e218d8a6fc2d6ee6e44291468a4c0d
Abrar <ifaz.farha@gmail.com>
2025-04-14 19:31:27 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/kazi-abrar-mahmud/', // Update this to match your repository name
+  //base: '/kazi-abrar-mahmud/', // Update this to match your repository name
 
   optimizeDeps: {
     exclude: ['lucide-react'],
